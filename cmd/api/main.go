90e86d48e5bfab619340a30433c4a558a799544c
thomas.queiroz <thomas.queiroz@idealctvm.com.br>
2026-04-29 19:18:24 -0300

    feat: add initial API and database configuration with Docker support

---
+++ cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+	//"github.com/thomasaqx/task-manager/internal/controller"
+
+	
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.Default()
+
+	//TODO: Registrar Rotas
+	// r.GET("", controller.getTodo)
+
+	r.Run(":" + port)
+	
+
+}
