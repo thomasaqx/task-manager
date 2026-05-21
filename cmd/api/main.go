4cf5f77970f942db9c2254965b6a7b8c7e952ff5
Thomas Queiroz <thomas.queiroz+ideal@idealctvm.com.br>
2026-05-21 10:55:44 -0300

    refactor: reorganize main function for improved readability and structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,27 +5,47 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	//"github.com/thomasaqx/task-manager/internal/controller"
+	"github.com/thomasaqx/task-manager/internal/controller"
 	"github.com/thomasaqx/task-manager/internal/db"
+	"github.com/thomasaqx/task-manager/internal/repository"
+	"github.com/thomasaqx/task-manager/internal/routes"
+	"github.com/thomasaqx/task-manager/internal/service"
 )
 
 func main() {
 
+	//database variable
+	db := db.ConnectDb()
+	
+	//repositories
+	userRepo := repository.NewUserRepository(db)
+	categoryRepo := repository.NewCategoryRepository(db)
+	taskRepo := repository.NewTaskRepository(db)
+
+	//services
+	userSvc := service.NewUserService(userRepo)
+	categorySvc := service.NewCategoryService(categoryRepo)
+	taskSvc := service.NewTaskService(taskRepo)
+
+	//controllers 
+	userCtrl := controller.NewUserController(userSvc)
+	categoryCtrl := controller.NewCategoryController(categorySvc)
+	taskCtrl := controller.NewTaskController(taskSvc)
+	
+	r := gin.Default()
+
+	//routes
+	routes.RegisterUserRoutes(r, userCtrl)
+	routes.RegisterCategoryRoutes(r, categoryCtrl)
+	routes.RegisterTaskRoutes(r, taskCtrl)
+
+
+	//init server
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "8080"
 	}
-
-	r := gin.Default()
-
-	//TODO: Register routes
-	// r.GET("", controller.getTodo)
-
-	
-	db := db.ConnectDb()
-	_ = db
-
 	//booting the server
 	r.Run(":" + port)
 
